d6f138a3f397e0a3ff679ec07b9d950d32e688ea
GaryLin <garylin2@anuegroup.com.tw>
2024-10-29 15:45:46 +0800

    style: update layout to use flex for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import cn from '@/utils/cn';
 import Container from '@/components/atoms/container';
 
 const HomePage = () => {
     return (
-        <main>
-            <Container>123</Container>
+        <main className="flex-grow">
+            <Container className="h-full">
+                <div className={cn('h-full w-full', 'text-center', 'flex flex-col justify-center', 'text-8xl')}>
+                    <p>Frontend</p>
+                    <p>Developer</p>
+                </div>
+            </Container>
         </main>
     );
 };
