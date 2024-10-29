7fbe684fc5dddb3bc7584dcf74db89a26ec4212d
GaryLin <garylin2@anuegroup.com.tw>
2024-10-29 17:12:55 +0800

    feat: improve responsiveness by adding DisplayElement component and updating layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,14 @@ const HomePage = () => {
         <main className="flex-grow">
             <Container className="h-full">
                 <div className={cn('h-full w-full', 'flex items-center justify-center', 'text-2xl')}>
-                    <DisplayElement tag="main">
-                        <DisplayElement tag="span">
-                            <p>Frontend</p>
-                            <p>Developer</p>
+                    <DisplayElement tag="body">
+                        <DisplayElement tag="main">
+                            <DisplayElement tag="h1" wrap={false}>
+                                <p className="inline-block">Hi, my name is Emily.</p>
+                            </DisplayElement>
+                            <DisplayElement tag="p" wrap={false}>
+                                <p className="inline-block">I am a frontend developer.</p>
+                            </DisplayElement>
                         </DisplayElement>
                     </DisplayElement>
                 </div>
--- src/components/atoms/display-element/index.tsx
+++ src/components/atoms/display-element/index.tsx
@@ -4,14 +4,33 @@ import { DivProps } from '@/interfaces/html';
 interface DisplayElementProps extends DivProps {
     tag?: keyof JSX.IntrinsicElements | ''; // 限制 tag 屬性為合法的 HTML 標籤
     tagClassName?: string;
+    wrap?: boolean; // 控制是否換行
 }
 
-const DisplayElement = ({ tag = '', tagClassName, className, children, ...props }: DisplayElementProps) => {
+const DisplayElement = ({
+    tag = '',
+    tagClassName,
+    wrap = true,
+    className,
+    children,
+    ...props
+}: DisplayElementProps) => {
     return (
         <div className={cn('whitespace-pre', className)} {...props}>
-            <p className={tagClassName}>{`<${tag}>`}</p>
-            <div className="pl-[2em]">{children}</div>
-            <p className={tagClassName}>{`</${tag}>`}</p>
+            {wrap && (
+                <>
+                    <p className={tagClassName}>{`<${tag}>`}</p>
+                    <div className="pl-[2em]">{children}</div>
+                    <p className={tagClassName}>{`</${tag}>`}</p>
+                </>
+            )}
+            {!wrap && (
+                <>
+                    <span className={cn('pl-[2em] pr-2', tagClassName)}>{`<${tag}>`}</span>
+                    {children}
+                    <span className={cn('pl-2', tagClassName)}>{`</${tag}>`}</span>
+                </>
+            )}
         </div>
     );
 };
