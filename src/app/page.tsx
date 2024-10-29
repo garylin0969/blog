f0a346e4255d018fdb91cd80111fc74baac9d756
GaryLin <garylin2@anuegroup.com.tw>
2024-10-29 16:42:07 +0800

    feat: add DisplayElement component and update layout for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import cn from '@/utils/cn';
 import Container from '@/components/atoms/container';
+import DisplayElement from '@/components/atoms/display-element';
 
 const HomePage = () => {
     return (
         <main className="flex-grow">
             <Container className="h-full">
-                <div className={cn('h-full w-full', 'text-center', 'flex flex-col justify-center', 'text-8xl')}>
-                    <p>Frontend</p>
-                    <p>Developer</p>
+                <div className={cn('h-full w-full', 'flex items-center justify-center', 'text-2xl')}>
+                    <DisplayElement tag="main">
+                        <DisplayElement tag="span">
+                            <p>Frontend</p>
+                            <p>Developer</p>
+                        </DisplayElement>
+                    </DisplayElement>
                 </div>
             </Container>
         </main>
