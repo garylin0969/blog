f46d8ee5eb0eed140b6457e8362a4f2e0cc6df74
Gary <garylin0969@gmail.com>
2024-10-31 23:18:43 +0800

    refactor: update about page layout and content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,26 @@
 import Avatar from '@/components/atoms/avatar';
+import cn from '@/utils/cn';
+import { IoMdMail } from 'react-icons/io';
+import { FaBuilding } from 'react-icons/fa';
+import Container from '@/components/atoms/container';
 
 const AboutPage = () => {
     return (
-        <div>
+        <Container>
             <Avatar />
-        </div>
+            <div className={cn('flex items-center', 'text-sm text-white')}>
+                <div className="mr-2">
+                    <FaBuilding />
+                </div>
+                <div>Anue</div>
+            </div>
+            <div className={cn('flex items-center', 'text-sm text-white')}>
+                <div className="mr-2">
+                    <IoMdMail />
+                </div>
+                <div>garylin0969@gmail.com</div>
+            </div>
+        </Container>
     );
 };
 
