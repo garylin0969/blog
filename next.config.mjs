c99c4fc56aa524251a4790544c51d05579bef090
GaryLin <garylin2@anuegroup.com.tw>
2024-11-05 17:17:55 +0800

    feat: add image domain to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['media.cakeresume.com'],
+    },
+};
 
 export default nextConfig;
