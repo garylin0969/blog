c67de4a58d865e6605217c46cbd598f6cbc5cd26
GaryLin <garylin2@anuegroup.com.tw>
2024-11-05 11:20:59 +0800

    refactor: update tag color in display-element component

--- src/components/molecules/display-element/index.tsx
+++ src/components/molecules/display-element/index.tsx
@@ -1,7 +1,7 @@
 import cn from '@/utils/cn';
 import { DivPropsT } from '@/interfaces/html';
 
-const tagColor = 'text-slate-400';
+const tagColor = 'text-slate-400 dark:text-slate-300';
 const tagTextColor = 'text-red-400';
 
 interface RenderTagProps {
