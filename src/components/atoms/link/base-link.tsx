862f339d7cc4ff4fe540d7dc1001b41779845c56
GaryLin <garylin2@anuegroup.com.tw>
2024-11-08 16:14:16 +0800

    refactor: remove IconLink component and add BaseLink component
    This commit removes the IconLink component and replaces it with the BaseLink component. The IconLink component was deleted, and the BaseLink component was added to handle link functionality. This change improves code organization and consistency in the project.

---
+++ src/components/atoms/link/base-link.tsx
@@ -0,0 +1,18 @@
+import { LinkPropsT } from '@/interfaces/html';
+import cn from '@/utils/cn';
+
+const BaseLink = ({ href, className, children, ...props }: LinkPropsT) => {
+    return (
+        <a
+            href={href}
+            className={cn('hover:text-sky-500 dark:hover:text-sky-400', className)}
+            target="_blank"
+            rel="noreferrer noopener"
+            {...props}
+        >
+            {children}
+        </a>
+    );
+};
+
+export default BaseLink;
