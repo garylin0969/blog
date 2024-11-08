0065ac98735f846127b11ad242e447005a3c6760
GaryLin <garylin2@anuegroup.com.tw>
2024-11-08 15:41:10 +0800

    refactor: update AboutPage component to use Experience component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,7 @@ import cn from '@/utils/cn';
 import Container from '@/components/atoms/container';
 import InfoCard from '@/components/organisms/info-card';
 import AboutInfo from '@/components/organisms/about-info';
-import DividerTitle from '@/components/molecules/divider-title';
-import ExperienceItem from '@/components/molecules/experience-item';
-import { experienceConfig } from '@/constants/personal';
+import Experience from '@/components/organisms/experience';
 
 const AboutPage = () => {
     return (
@@ -15,24 +13,7 @@ const AboutPage = () => {
             <div className="mx-auto max-w-3xl">
                 <AboutInfo />
             </div>
-            <DividerTitle className="my-8" align="center" title="Experience" />
-            <div className={cn('relative', 'flex-col items-center md:flex')}>
-                <div className={cn('absolute left-1/2', 'h-full', 'border border-gray-400', 'hidden md:block')} />
-                {experienceConfig?.map((data) => {
-                    return (
-                        <ExperienceItem
-                            key={data?.companyName}
-                            className="md:odd:translate-x-[-60%] md:even:translate-x-[60%]"
-                            logoSrc={data?.logoSrc}
-                            title={data?.title}
-                            companyUrl={data?.companyUrl}
-                            companyName={data?.companyName}
-                            employmentStartDate={data?.employmentStartDate}
-                            employmentEndDate={data?.employmentEndDate}
-                        />
-                    );
-                })}
-            </div>
+            <Experience />
         </Container>
     );
 };
