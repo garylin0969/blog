1214bc99edbf7cd04507914aebd656eacb3e5791
GaryLin <garylin2@anuegroup.com.tw>
2024-11-11 15:16:25 +0800

    style: add cursor-pointer to BaseLink component

--- src/components/atoms/link/base-link.tsx
+++ src/components/atoms/link/base-link.tsx
@@ -5,7 +5,7 @@ const BaseLink = ({ href, className, children, ...props }: LinkPropsT) => {
     return (
         <a
             href={href}
-            className={cn('hover:text-sky-500 dark:hover:text-sky-400', className)}
+            className={cn('cursor-pointer hover:text-sky-500 dark:hover:text-sky-400', className)}
             target="_blank"
             rel="noreferrer noopener"
             {...props}
