6900669cb538c4c2a922cfe195dbaa0d14e0069c
GaryLin <garylin2@anuegroup.com.tw>
2024-11-12 09:25:13 +0800

    feat: add SectionPropsT type to html.ts

--- src/interfaces/html.ts
+++ src/interfaces/html.ts
@@ -8,4 +8,6 @@ export type SpanPropsT = HTMLAttributes<HTMLSpanElement>;
 
 export type LinkPropsT = AnchorHTMLAttributes<HTMLAnchorElement>;
 
+export type SectionPropsT = HTMLAttributes<HTMLElement>;
+
 export type SvgPropsT = HTMLAttributes<SVGElement>;
