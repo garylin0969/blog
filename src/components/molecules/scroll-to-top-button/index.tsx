35289edc4e898f36dd8fd1ff10fa5ccb00bb0c69
GaryLin <garylin2@anuegroup.com.tw>
2024-11-12 14:21:03 +0800

    refactor: optimize ScrollToTopButton component
    Simplify the ScrollToTopButton component by using the useState and useEffect hooks from React. Also, adjust the position and opacity transition of the button for a better user experience.

--- src/components/molecules/scroll-to-top-button/index.tsx
+++ src/components/molecules/scroll-to-top-button/index.tsx
@@ -1,27 +1,42 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 import cn from '@/utils/cn';
 
+const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
 const ScrollToTopButton = () => {
-    const handleScrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        window.scrollY > 0 ? setIsVisible(true) : setIsVisible(false);
     };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <button
             onClick={handleScrollToTop}
             className={cn(
                 'group',
-                'fixed bottom-6 right-6',
+                'fixed bottom-12 right-12',
                 'h-12 w-12 rounded-full',
                 'bg-white text-slate-800',
                 'flex items-center justify-center',
                 'shadow-sm dark:shadow-slate-400',
-                'transition-colors duration-300',
+                'transition-[opacity] duration-200',
+                isVisible ? 'opacity-100' : 'pointer-events-none opacity-0',
             )}
         >
-            <FaArrowUp className="transition-transform duration-300 group-hover:scale-125" />
+            <FaArrowUp className="transition-transform duration-300 group-hover:scale-150" />
         </button>
     );
 };
