742ea6a624f7177cbf2dfb2fbabf5def46daf803
GaryLin <garylin2@anuegroup.com.tw>
2024-11-13 14:47:10 +0800

    feat: add BlogAside component for blog page

---
+++ src/components/organisms/blog-aside/index.tsx
@@ -0,0 +1,19 @@
+import cn from '@/utils/cn';
+import InfoCard from '@/components/molecules/info-card';
+import DividerTitle from '@/components/molecules/divider-title';
+import ArticleCategories from '@/components/molecules/article-categories';
+import { AsidePropsT } from '@/interfaces/html';
+
+const BlogAside = ({ className, ...props }: AsidePropsT) => {
+    return (
+        <aside className={cn('hidden flex-col md:flex', 'px-6', className)} {...props}>
+            <InfoCard className="mt-8" />
+            <DividerTitle align="center" title="文章分類" className="my-5 md:text-xl" />
+            <div className="flex-grow overflow-y-scroll">
+                <ArticleCategories className="space-y-3 text-center" />
+            </div>
+        </aside>
+    );
+};
+
+export default BlogAside;
