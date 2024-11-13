c943a2cf4d6c13962c38367e77453d967cd4497b
GaryLin <garylin2@anuegroup.com.tw>
2024-11-13 14:47:18 +0800

    refactor: optimize layout and styling of Blog page component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,41 +2,16 @@ import cn from '@/utils/cn';
 import Container from '@/components/atoms/container';
 import InfoCard from '@/components/molecules/info-card';
 import DividerTitle from '@/components/molecules/divider-title';
-import { BaseLink } from '@/components/atoms/link';
+import ArticleCategories from '@/components/molecules/article-categories';
+import BlogAside from '@/components/organisms/blog-aside';
 
 const Blog = () => {
     return (
         <Container className="flex h-full">
-            <div className="h-[1500px] flex-grow">
-                <ul className="my-2 flex flex-wrap gap-4 overflow-x-scroll border-y p-[10px]">
-                    <li>
-                        <BaseLink>All</BaseLink>
-                    </li>
-                    <li>
-                        <BaseLink>Frontend</BaseLink>
-                    </li>
-                    <li>
-                        <BaseLink>Life</BaseLink>
-                    </li>
-                </ul>
+            <div className="flex-grow">
+                <ArticleCategories className={cn('my-2 p-[10px]', 'border-y', 'flex flex-wrap gap-4')} />
             </div>
-            <aside className="sticky top-16 hidden max-h-[calc(100svh-132px)] w-[368px] flex-col px-6 md:flex">
-                <InfoCard className="mt-8" />
-                <DividerTitle align="center" title="文章分類" className="my-5 md:text-xl" />
-                <div className="flex-grow overflow-y-scroll">
-                    <ul className="space-y-3 text-center">
-                        <li>
-                            <BaseLink>All</BaseLink>
-                        </li>
-                        <li>
-                            <BaseLink>Frontend</BaseLink>
-                        </li>
-                        <li>
-                            <BaseLink>Life</BaseLink>
-                        </li>
-                    </ul>
-                </div>
-            </aside>
+            <BlogAside className="sticky top-16 max-h-[calc(100svh-132px)] w-80" />
         </Container>
     );
 };
