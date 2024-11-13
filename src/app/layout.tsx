f5af6dd695bef8d1049df3d18cdd1f37ee88da2e
GaryLin <garylin2@anuegroup.com.tw>
2024-11-13 16:22:30 +0800

    refactor: optimize layout and styling of RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,7 @@ export const metadata: Metadata = {
     },
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html suppressHydrationWarning lang="zh-TW">
             <head>
