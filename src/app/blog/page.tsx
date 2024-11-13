df59cfcf038d9f4674546bb593972192f11012ec
GaryLin <garylin2@anuegroup.com.tw>
2024-11-13 16:27:29 +0800

    feat: add ArticleList component for displaying a list of articles

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,26 +1,10 @@
-import cn from '@/utils/cn';
-import Container from '@/components/atoms/container';
-import ArticleCategories from '@/components/molecules/article-categories';
-import BlogAside from '@/components/organisms/blog-aside';
-import ArticleCard from '@/components/molecules/article-card';
+import ArticleList from '@/components/organisms/article-list';
 
 const Blog = () => {
     return (
-        <Container className="flex h-full">
-            <div className="flex-grow">
-                <ArticleCategories className={cn('my-2 p-[10px]', 'border-y', 'flex flex-wrap gap-4 md:hidden')} />
-                <div>
-                    {[1, 2, 3, 4, 5, 6, 7, 8, 9]?.map((value) => {
-                        return (
-                            <div key={value} className="flex justify-center border-b">
-                                <ArticleCard />
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-            <BlogAside className="sticky top-16 max-h-[calc(100svh-132px)] w-80" />
-        </Container>
+        <>
+            <ArticleList />
+        </>
     );
 };
 
