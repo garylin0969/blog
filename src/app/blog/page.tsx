8d3f82c528f1bc40665fd68af866179268f0f800
GaryLin <garylin2@anuegroup.com.tw>
2024-11-13 11:25:53 +0800

    refactor: optimize layout and styling of Blog page component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,14 +2,40 @@ import cn from '@/utils/cn';
 import Container from '@/components/atoms/container';
 import InfoCard from '@/components/molecules/info-card';
 import DividerTitle from '@/components/molecules/divider-title';
+import { BaseLink } from '@/components/atoms/link';
 
 const Blog = () => {
     return (
         <Container className="flex h-full">
-            <div className="h-full flex-grow"></div>
-            <aside className="h-full w-[368px] border-l px-6">
-                <InfoCard className="mt-10" />
-                <DividerTitle title="文章分類" className="my-5 md:text-2xl" />
+            <div className="h-full flex-grow">
+                <ul className="my-2 flex flex-wrap gap-4 overflow-x-scroll border-y p-[10px]">
+                    <li>
+                        <BaseLink>All</BaseLink>
+                    </li>
+                    <li>
+                        <BaseLink>Frontend</BaseLink>
+                    </li>
+                    <li>
+                        <BaseLink>Life</BaseLink>
+                    </li>
+                </ul>
+            </div>
+            <aside className="hidden max-h-[calc(100svh-132px)] w-[368px] flex-col px-6 md:flex">
+                <InfoCard className="mt-8" />
+                <DividerTitle align="center" title="文章分類" className="my-5 md:text-xl" />
+                <div className="flex-grow overflow-y-scroll">
+                    <ul className="space-y-3 text-center">
+                        <li>
+                            <BaseLink>All</BaseLink>
+                        </li>
+                        <li>
+                            <BaseLink>Frontend</BaseLink>
+                        </li>
+                        <li>
+                            <BaseLink>Life</BaseLink>
+                        </li>
+                    </ul>
+                </div>
             </aside>
         </Container>
     );
