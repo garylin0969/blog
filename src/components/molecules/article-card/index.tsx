8cc90e5340d9cfe76341307360fdd48b77b6ebe3
GaryLin <garylin2@anuegroup.com.tw>
2024-11-15 15:24:48 +0800

    refactor: update article card component to use NextLink for post and category links

--- src/components/molecules/article-card/index.tsx
+++ src/components/molecules/article-card/index.tsx
@@ -1,42 +1,50 @@
 import Image from 'next/image';
 import cn from '@/utils/cn';
 import { ArticlePropsT } from '@/interfaces/html';
+import { NextLink } from '@/components/atoms/link';
 import { Post } from '@/interfaces/post';
-import { BaseLink } from '@/components/atoms/link';
-import DateFormatter from '@/app/_components/date-formatter';
+import { DateFormatter } from '@/components/atoms/date';
 
 interface ArticleCardProps extends ArticlePropsT {
     post: Post;
 }
 
 const ArticleCard = ({ className, post, ...props }: ArticleCardProps) => {
+    const { category, slug, title, date, excerpt, coverImage } = post;
+
+    const postLink = `/blog/post/${slug}`;
+    const categoryLink = `/blog/${category?.toLowerCase()}`;
+
     return (
         <article className={cn('p-5', className)} {...props}>
             <div className="flex gap-x-5">
                 <div className={cn('w-3/4', 'space-y-2')}>
-                    <BaseLink className={cn('inline-block', 'text-lg font-bold', 'md:text-2xl', 'dark:text-white')}>
-                        <h2>{post?.title}</h2>
-                    </BaseLink>
+                    <NextLink
+                        href={postLink}
+                        className={cn('inline-block', 'text-lg font-bold', 'md:text-2xl', 'dark:text-white')}
+                    >
+                        <h2>{title}</h2>
+                    </NextLink>
                     <div className={cn('font-serif', 'space-x-3')}>
                         <span>
-                            <DateFormatter dateString={post?.date} />
+                            <DateFormatter dateString={date} />
                         </span>
                         <span>__</span>
                         <span>
-                            <BaseLink>{post?.category}</BaseLink>
+                            <NextLink href={categoryLink}>{category}</NextLink>
                         </span>
                     </div>
-                    <h3 className="line-clamp-2 text-base/7" title={post?.excerpt}>
-                        {post?.excerpt}
+                    <h3 className="line-clamp-2 text-base/7" title={excerpt}>
+                        {excerpt}
                     </h3>
                 </div>
                 <div className="w-1/4">
                     <Image
-                        src={post?.coverImage}
+                        src={coverImage}
                         className={cn('mx-auto object-cover', 'h-[53px] w-20', 'md:h-[107px] md:w-40')}
                         width={160}
                         height={107}
-                        alt=""
+                        alt={title}
                     />
                 </div>
             </div>
