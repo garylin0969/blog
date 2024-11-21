0766147f1e4a0044c93b330f7907b0b041685c06
GaryLin <garylin2@anuegroup.com.tw>
2024-11-21 11:29:54 +0800

    refactor: optimize blog configuration and layout
    - Remove unused articleCategoriesConfig constant
    - Remove markdownToHtml utility function
    - Optimize post retrieval and filtering functions
    - Update blog configuration in layout.tsx to use blog.config.json
    - Remove unused constants in constants.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '@/lib/constants';
+import config from 'blog.config.json';
 import cn from '@/utils/cn';
 import ThemeProvider from '@/providers/theme-provider';
 import Header from '@/components/organisms/header';
@@ -18,37 +18,24 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-    title: CMS_NAME,
-    description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
-    openGraph: {
-        images: [HOME_OG_IMAGE_URL],
-    },
+    ...config.metadata,
+    // openGraph: {
+    //     images: [HOME_OG_IMAGE_URL],
+    // },
 };
+
+const light = `text-[${config.theme.light.text}] bg-[${config.theme.light.background}]`;
+const dark = `dark:text-[${config.theme.dark.text}] dark:bg-[${config.theme.dark.background}]`;
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html suppressHydrationWarning lang="zh-TW">
             <head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-                <link rel="manifest" href="/favicon/site.webmanifest" />
-                <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
-                <link rel="shortcut icon" href="/favicon/favicon.ico" />
-                <meta name="msapplication-TileColor" content="#000000" />
-                <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-                <meta name="theme-color" content="#000" />
-                <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+                <link rel="icon" href="/assets/logo.svg'" sizes="any" />
             </head>
             <body className={cn(inter.className, 'antialiased')}>
                 <ThemeProvider>
-                    <div
-                        className={cn(
-                            'transition-colors duration-300',
-                            'text-slate-700 dark:text-slate-300',
-                            'dark:bg-slate-900',
-                        )}
-                    >
+                    <div className={cn('transition-colors duration-300', light, dark)}>
                         <Header />
                         <div className="flex min-h-[calc(100vh-68px)] pt-16">
                             <main className="flex-grow">{children}</main>
