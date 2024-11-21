b6de10ddd3b7bbd4e2e743e17d10f2009b89571e
GaryLin <garylin2@anuegroup.com.tw>
2024-11-21 10:06:38 +0800

    refactor: Remove unused components and styles

--- src/app/_components/container.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-    children?: React.ReactNode;
-};
-
-const Container = ({ children }: Props) => {
-    return <div className="mx-auto max-w-6xl px-4">{children}</div>;
-};
-
-export default Container;
