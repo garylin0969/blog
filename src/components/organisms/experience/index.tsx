056f18d08609faf62791357d55b28b6a7447467f
GaryLin <garylin2@anuegroup.com.tw>
2024-11-22 12:26:22 +0800

    refactor: optimize layout and update metadata in about page component

--- src/components/organisms/experience/index.tsx
+++ src/components/organisms/experience/index.tsx
@@ -6,8 +6,8 @@ import { ExperienceConfig } from '@/constants/personal';
 const Experience = ({ className, ...props }: SectionPropsT) => {
     return (
         <>
-            <section className={cn('flex justify-center', className)} {...props}>
-                <ul className={cn('relative', 'space-y-4')}>
+            <section className={cn('grid place-content-center', className)} {...props}>
+                <ul className={cn('relative')}>
                     <div
                         className={cn(
                             'hidden md:block',
@@ -16,33 +16,38 @@ const Experience = ({ className, ...props }: SectionPropsT) => {
                             'border border-sky-500 dark:border-sky-400',
                         )}
                     />
-                    {ExperienceConfig?.map((data) => {
-                        return (
-                            <li key={data?.companyName} className={cn('relative', 'md:grid md:grid-cols-2', 'group')}>
-                                <div
-                                    className={cn(
-                                        'hidden md:block',
-                                        'rounded-full',
-                                        'bg-sky-500 dark:bg-sky-400',
-                                        'absolute left-1/2 top-1/2 h-4 w-4 translate-x-[-50%] translate-y-[-50%]',
-                                    )}
-                                />
-                                <ExperienceItem
-                                    className={cn(
-                                        'm-1 p-2 md:m-3',
-                                        'md:group-even:col-end-3',
-                                        // 'md:group-odd:justify-end',
-                                    )}
-                                    logoSrc={data?.logoSrc}
-                                    title={data?.title}
-                                    companyUrl={data?.companyUrl}
-                                    companyName={data?.companyName}
-                                    employmentStartDate={data?.employmentStartDate}
-                                    employmentEndDate={data?.employmentEndDate}
-                                />
-                            </li>
-                        );
-                    })}
+                    <div className="space-y-4">
+                        {ExperienceConfig?.map((data) => {
+                            return (
+                                <li
+                                    key={data?.companyName}
+                                    className={cn('relative', 'md:grid md:grid-cols-2', 'group')}
+                                >
+                                    <div
+                                        className={cn(
+                                            'hidden md:block',
+                                            'rounded-full',
+                                            'bg-sky-500 dark:bg-sky-400',
+                                            'absolute left-1/2 top-1/2 h-4 w-4 translate-x-[-50%] translate-y-[-50%]',
+                                        )}
+                                    />
+                                    <ExperienceItem
+                                        className={cn(
+                                            'm-1 p-2 md:m-3',
+                                            'md:group-even:col-end-3',
+                                            // 'md:group-odd:justify-end',
+                                        )}
+                                        logoSrc={data?.logoSrc}
+                                        title={data?.title}
+                                        companyUrl={data?.companyUrl}
+                                        companyName={data?.companyName}
+                                        employmentStartDate={data?.employmentStartDate}
+                                        employmentEndDate={data?.employmentEndDate}
+                                    />
+                                </li>
+                            );
+                        })}
+                    </div>
                 </ul>
             </section>
         </>
