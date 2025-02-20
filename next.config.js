8e46c1e09cc0717f5ae84ae76b5d430f87e811f7
GaryLin <garylin2@anuegroup.com.tw>
2025-02-20 10:05:55 +0800

    feat: add Giscus comments support with Next.js image configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const { withContentlayer } = require('next-contentlayer');
 
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['giscus.app'],
+    },
+};
 
 module.exports = withContentlayer(nextConfig);
