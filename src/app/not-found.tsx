86520ae241cb4bd49fbbf6f96dafe3eba231c933
Gary <garylin0969@gmail.com>
2025-02-23 04:09:50 +0800

    refactor: update 404 page with dark mode and NextLink component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
+import { NextLink } from '@/components/atoms/link';
 import { BaseButton } from '@/components/atoms/button';
 
 const NotFound = () => {
     return (
         <div className="grid h-full place-content-center px-4">
             <div className="text-center">
-                <h2 className="text-9xl font-black text-gray-200">404</h2>
-                <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
+                <h2 className="text-9xl font-black dark:text-gray-200">404</h2>
+                <p className="text-2xl font-bold tracking-tight sm:text-4xl">Uh-oh!</p>
                 <p className="mt-4 text-gray-500">We can't find that page.</p>
-                <Link href="/">
+                <NextLink href="/">
                     <BaseButton className="mt-6">Go Back Home</BaseButton>
-                </Link>
+                </NextLink>
             </div>
         </div>
     );
