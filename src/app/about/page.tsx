4ef5ce6d0877bcfadaea354e62da09cf729d9fe2
Gary <garylin0969@gmail.com>
2025-02-23 23:47:45 +0800

    feat: add Skills section to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import InfoCard from '@/components/molecules/info-card';
 import AboutInfo from '@/components/organisms/about-info';
 import DividerTitle from '@/components/molecules/divider-title';
 import Experience from '@/components/organisms/experience';
+import Skills from '@/components/organisms/skills';
 import Projects from '@/components/organisms/projects';
 
 export const metadata: Metadata = {
@@ -31,6 +32,8 @@ const AboutPage = () => {
             <AboutInfo className="mx-auto max-w-3xl" />
             <DividerTitle align="center" title="Experience" />
             <Experience />
+            <DividerTitle align="center" title="Skills" />
+            <Skills />
             <DividerTitle align="center" title="Projects" />
             <Projects className="pb-6" />
         </Container>
