a0ef23e7809c40bab5d7dc4085b16ec15a9f2a95
Gary <garylin0969@gmail.com>
2025-02-23 15:54:34 +0800

    refactor: remove Instagram social link

--- src/constants/personal.tsx
+++ src/constants/personal.tsx
@@ -42,10 +42,10 @@ export const SocialIconLinksConfig = [
         link: 'https://www.facebook.com/profile.php?id=100009915255579&mibextid=ZbWKwL',
         icon: <FaFacebook className="cursor-pointer opacity-90" />,
     },
-    {
-        link: 'https://www.instagram.com/gary_1226_/',
-        icon: <FaInstagram className="cursor-pointer opacity-90" />,
-    },
+    // {
+    //     link: 'https://www.instagram.com/gary_1226_/',
+    //     icon: <FaInstagram className="cursor-pointer opacity-90" />,
+    // },
 ];
 
 export const HeaderSocialIconLinksConfig = [
