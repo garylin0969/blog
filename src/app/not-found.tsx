a7f45ee31710ad758c2135fc09cfbdb7b55f4302
Gary <garylin0969@gmail.com>
2025-02-23 13:57:03 +0800

    fix: remove period from 404 page error message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
             <div className="text-center">
                 <h2 className="text-9xl font-black dark:text-gray-200">404</h2>
                 <p className="text-2xl font-bold tracking-tight sm:text-4xl">Uh-oh!</p>
-                <p className="mt-4 text-gray-500">We can't find that page.</p>
+                <p className="mt-4 text-gray-500">We can't find that page</p>
                 <NextLink href="/">
                     <BaseButton className="mt-6">Go Back Home</BaseButton>
                 </NextLink>
