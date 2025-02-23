8706baf92255a85f59a7d9f9b493548655450bd0
Gary <garylin0969@gmail.com>
2025-02-23 15:50:04 +0800

    refactor: update footer copyright text and use site title

--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,18 +1,20 @@
 import cn from '@/utils/cn';
 import { BaseLink } from '@/components/atoms/link';
-import { author } from '@/constants/personal';
+import { siteTitle } from '@/constants/header';
 
-const year = new Date().getFullYear();
+const startYear = 2024;
+const currentYear = new Date().getFullYear();
 const link = 'https://github.com/garylin0969';
 
 const Footer = () => {
     return (
         <footer className={cn('py-6', 'text-center text-sm tracking-wide')}>
             <p>
-                &copy; {year} Developed by
-                <span className={cn('ml-1')}>
-                    <BaseLink href={link}>{author}</BaseLink>
+                &copy; {startYear}-{currentYear}
+                <span className={cn('mx-1')}>
+                    <BaseLink href={link}>{siteTitle}</BaseLink>
                 </span>
+                | All Rights Reserved.
             </p>
         </footer>
     );
