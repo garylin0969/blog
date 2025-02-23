3801a498041daba5a816128f7c5121a808e51098
Gary <garylin0969@gmail.com>
2025-02-24 01:24:37 +0800

    refactor: remove hover scale effect from Skills section card

--- src/components/organisms/skills/index.tsx
+++ src/components/organisms/skills/index.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
             {SkillsConfig.map(({ title, skills }) => (
                 <div
                     key={title}
-                    className="mx-2 flex-1 transform rounded-lg border border-purple-100 p-5 shadow-md transition-transform hover:scale-105 dark:border-white/30"
+                    className="mx-2 flex-1 transform rounded-lg border border-purple-100 p-5 shadow-md transition-transform dark:border-white/30"
                 >
                     <h2 className="border-b-2 pb-2 text-xl font-bold">{title}</h2>
                     <ul className={cn('list-inside list-disc space-y-1 rounded-lg', 'p-4')}>
