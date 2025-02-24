d0c105fae7aaaa592d3d154725283e826994f3fc
Gary <garylin0969@gmail.com>
2025-02-24 20:32:57 +0800

    refactor: improve ArticleCategories component null safety and category handling

--- src/components/molecules/article-categories/index.tsx
+++ src/components/molecules/article-categories/index.tsx
@@ -4,6 +4,8 @@ import { UlPropsT } from '@/interfaces/html';
 import { getAllCategories } from '@/utils/posts';
 
 const ArticleCategories = ({ className, ...props }: UlPropsT) => {
+    const categories = getAllCategories() || []; // 確保不會是 undefined
+
     return (
         <ul className={className} {...props}>
             <li>
@@ -11,9 +13,9 @@ const ArticleCategories = ({ className, ...props }: UlPropsT) => {
                     <BaseButton>All</BaseButton>
                 </NextLink>
             </li>
-            {getAllCategories()?.map((category) => (
+            {categories.map((category) => (
                 <li key={category}>
-                    <NextLink href={`/blog/${category?.toLowerCase()}`}>
+                    <NextLink href={`/blog/${category.toLowerCase()}`}>
                         <BaseButton>{category}</BaseButton>
                     </NextLink>
                 </li>
