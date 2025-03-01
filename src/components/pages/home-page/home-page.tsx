3a5c5decde0ef321d0f376943ad39afd0c5addfd
Gary <garylin0969@gmail.com>
2025-03-02 01:33:18 +0800

    style: adjust home page container and margin for responsive layout
    - Removed explicit padding from container
    - Added responsive top margin for mobile devices
    - Maintained overall flex and spacing layout

--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -4,8 +4,8 @@ import WebsiteIntroduction from '@/components/organisms/website-introduction';
 
 const HomePage = () => {
     return (
-        <Container className="h-full p-6">
-            <div className="flex h-full flex-col justify-evenly space-y-6">
+        <Container className="h-full">
+            <div className="mt-4 flex h-full flex-col justify-evenly space-y-6 md:mt-0">
                 <WebsiteIntroduction />
                 <LatestArticles />
             </div>
