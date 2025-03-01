09a1f7c157e5e308ca7c44952bb4a9454e253e89
Gary <garylin0969@gmail.com>
2025-03-01 17:00:03 +0800

    style: simplify 404 page text styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,9 @@ const NotFound = () => {
     return (
         <div className="grid h-full place-content-center px-4">
             <div className="text-center">
-                <h2 className="text-9xl font-black dark:text-gray-200">404</h2>
+                <h2 className="text-9xl font-black">404</h2>
                 <p className="text-2xl font-bold tracking-tight sm:text-4xl">Uh-oh!</p>
-                <p className="mt-4 text-gray-500">We can't find that page</p>
+                <p className="mt-4">We can't find that page</p>
                 <NextLink href="/">
                     <BaseButton className="mt-6">Go Back Home</BaseButton>
                 </NextLink>
