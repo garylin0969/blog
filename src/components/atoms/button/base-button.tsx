2948a0fe850e16910009d1196309d8433e3efd1c
Gary <garylin0969@gmail.com>
2025-03-01 17:09:40 +0800

    style: add font-semibold to base button text

--- src/components/atoms/button/base-button.tsx
+++ src/components/atoms/button/base-button.tsx
@@ -9,7 +9,7 @@ const BaseButton = forwardRef<HTMLButtonElement, ButtonPropsT>(({ className, chi
             className={cn(
                 'px-4 py-2',
                 'rounded-md',
-                'text-sm text-white',
+                'text-sm font-semibold text-white',
                 'bg-sky-500',
                 'hover:bg-sky-600 dark:hover:bg-sky-400',
                 className,
