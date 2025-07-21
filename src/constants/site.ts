5a0785405203362ff6d47c4797950a5b5b22d7d1
Gary Lin <garylin0969@gmail.com>
2025-07-21 10:07:50 +0800

    feat: enhance site constants with additional configuration options
    - Added new constants for website title, default image, Google Analytics ID, posts per page, and notice bar message with link.
    - Improved clarity and organization of site-related constants for better maintainability.

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,6 +1,21 @@
+// 網站網址
 export const DOMAIN = 'https://www.garylin.dev';
+
+// 網站圖示
 export const LOGO_IMAGE_PATH = '/favicons/favicon-32x32.png';
+
+// 網站標題
 export const WEBSITE_TITLE = 'garylin.dev';
+
+// 預設圖片
 export const DEFAULT_IMAGE_NOT_FOUND = '/image-not-found.png';
+
+// Google Analytics ID
 export const GA_ID = 'G-F0MRGZ2J39';
+
+// 文章每頁顯示數量
 export const POSTS_PER_PAGE = 10;
+
+// 公告訊息
+export const NOTICE_BAR_MESSAGE = '點我至 Hot Now｜熱門話題一把抓';
+export const NOTICE_BAR_LINK = 'https://hotnow.garylin.dev/';
