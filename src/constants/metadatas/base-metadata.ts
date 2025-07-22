8bc7c0665ccc100e095f52b44f2112b18ae50d7a
Gary <garylin0969@gmail.com>
2025-07-23 01:00:29 +0800

    feat: add Android favicon to base metadata
    - Included a new Android favicon with specified dimensions and type in the baseMetadata for improved branding across devices.

--- src/constants/metadatas/base-metadata.ts
+++ src/constants/metadatas/base-metadata.ts
@@ -74,6 +74,12 @@ export const baseMetadata: Metadata = {
         other: [
             {
                 rel: 'icon',
+                url: '/favicons/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                rel: 'icon',
                 url: '/favicons/android-chrome-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
